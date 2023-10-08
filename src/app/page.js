0b792c06343cea6af94064126c59f51c9c30268b
mauzorzan <mauricio@u.yale-nus.edu.sg>
2023-10-09 02:52:35 +0800

    fixes

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
-import Navbar from "./components/Navbar";
+import NavBar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
@@ -10,7 +10,7 @@ import Footer from "./components/Footer";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      <Navbar />
+      <NavBar />
       <div class="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
         <AboutSection />
